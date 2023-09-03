d224415ed74ee3fa022168ef64a7f8c0c5f661ea
Lim Xin Le <126428727+xLer0806@users.noreply.github.com>
2023-09-03 14:31:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c1cjuo7qubpg.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "limxinle-employee"
+customregion = "us-east-1"
+
